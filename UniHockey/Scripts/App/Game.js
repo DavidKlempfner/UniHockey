cab2c8cae861d10e2e215cec87143cf06869879c
AVANADE-CORP\david.klempfner <dklempfner@gmail.com>
2018-10-18 13:54:59 +1100

    Added clear textboxes button.

--- UniHockey/Scripts/App/Game.js
+++ UniHockey/Scripts/App/Game.js
@@ -11,12 +11,21 @@ function updateTeamScore(teamPlayerScoresDivId, teamScoreTextboxId) {
 
 function addPlayersScores(teamPlayerScoresDivId) {
     var team1PlayerScoresDiv = document.getElementById(teamPlayerScoresDivId);
-    var textboxes = team1PlayerScoresDiv.getElementsByTagName('input');
+    var textboxes = team1PlayerScoresDiv.getElementsByTagName("input");
     var currentScore = 0;
     for (var i = 0; i < textboxes.length; i++) {
-        if (textboxes[i].type == 'number') {
+        if (textboxes[i].type == "number") {
             currentScore += parseInt(textboxes[i].value);
         }
     }
     return currentScore;
+}
+
+function resetTextboxes() {
+    var elements = document.getElementsByTagName("input");
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type == "number") {
+            elements[i].value = "0";
+        }
+    }
 }
