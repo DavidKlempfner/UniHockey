1b8b8b313e28ffb06cfdd1f123c22830cf4d23f9
AVANADE-CORP\david.klempfner <dklempfner@gmail.com>
2019-01-12 16:55:18 +1100

    Made the tournament javascript unobtrusive. Now can only create a game if both team drop downs have a value selected.

--- UniHockey/Scripts/App/Tournament.js
+++ UniHockey/Scripts/App/Tournament.js
@@ -1,9 +1,23 @@
-﻿function updateDropDown(teamId) {
-    var defaultValue = '';
+﻿var defaultValue = '';
 
+window.onload = setupEventHandlers;
+
+function setupEventHandlers() {
+    var team1DropDown = document.getElementById('Team1Id');
+    team1DropDown.setAttribute('onchange', "updateDropDown('Team1Id')");
+
+    var team2DropDown = document.getElementById('Team2Id');
+    team2DropDown.setAttribute('onchange', "updateDropDown('Team2Id')");
+}
+
+function updateDropDown(teamId) {    
     var team1DropDown = document.getElementById('Team1Id');
     var team2DropDown = document.getElementById('Team2Id');
+    preventSameTeamSelection(team1DropDown, team2DropDown, teamId);
+    enableButtonIfValidState(team1DropDown, team2DropDown);
+}
 
+function preventSameTeamSelection(team1DropDown, team2DropDown, teamId) {
     if (team1DropDown.value == team2DropDown.value && team1DropDown.value != defaultValue) {
         if (teamId == 'Team1Id') {
             team2DropDown.value = defaultValue;
@@ -11,5 +25,15 @@
         else {
             team1DropDown.value = defaultValue;
         }
-    }    
+    }   
+}
+
+function enableButtonIfValidState(team1DropDown, team2DropDown) {
+    var submitButton = document.getElementById('Submit');
+    if (team1DropDown.value != defaultValue && team2DropDown.value != defaultValue) {
+        submitButton.removeAttribute('disabled');
+    }
+    else {
+        submitButton.setAttribute('disabled', 'disabled');
+    }
 }
